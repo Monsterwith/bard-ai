54a0b5b52e7aab0b3369043d5dc9051298e3a848
Monsterwith <122149565+Monsterwith@users.noreply.github.com>
2025-08-03 14:31:15 +0200

    Update askAI.js

--- examples/askAI.js
+++ examples/askAI.js
@@ -1,5 +1,5 @@
 import Bard, { askAI } from "bard-ai";
 
-await Bard.init("YOUR_COOKIE_KEY");
+await Bard.init("g.a000zggOsMrgcQ0YOvjrCvhyMaLKLF-JMP9rSi1bG1ALxOYCWtCnAU0xH_jYfejvgwoihQk80gACgYKAaYSARMSFQHGX2MiWW5g4-Pw1qxeAtRbzJX08xoVAUF8yKp5p8ZcLXm3-FiYeE-dbC8M0076");
 
 console.log(await askAI("Hello world!"));
