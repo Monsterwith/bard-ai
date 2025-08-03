944e8da7e63890a2895491b23ded8f337c2dfd3f
Monsterwith <122149565+Monsterwith@users.noreply.github.com>
2025-08-03 14:31:43 +0200

    Update export.js

--- examples/export.js
+++ examples/export.js
@@ -1,6 +1,6 @@
 import Bard from "bard-ai";
 
-await Bard.init("YOUR_COOKIE_KEY");
+await Bard.init("g.a000zggOsMrgcQ0YOvjrCvhyMaLKLF-JMP9rSi1bG1ALxOYCWtCnAU0xH_jYfejvgwoihQk80gACgYKAaYSARMSFQHGX2MiWW5g4-Pw1qxeAtRbzJX08xoVAUF8yKp5p8ZcLXm3-FiYeE-dbC8M0076");
 
 // ! Comment out code block underneath, and uncomment this
 let myConversation = new Bard.Chat();
